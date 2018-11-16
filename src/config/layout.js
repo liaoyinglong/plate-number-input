7ea7806ffccf1b1b82777fd664e2f089dbf05631
liaoyinglong <vigossliao@gmail.com>
2018-11-16 20:21:13 +0800

    fix(build): 使用import方式引入，module字段对webpack的影响

--- src/config/layout.js
+++ src/config/layout.js
@@ -1,9 +1,9 @@
-require('core-js/fn/array/fill')
-const input = new Array(8).fill(undefined).map((item, index) => `<span data-index='${index}'></span>`)
+require('core-js/fn/array/fill');
+const input = new Array(8).fill(undefined).map((item, index) => `<span data-index='${index}'></span>`);
 const inputbox = `
  <div>
   <div id='inputboxWrapper' class="container-top">
-  <div class='container-info'>宝立方创意产业园(地库)</div>
+  <div class='container-info'>请输入您的车牌</div>
   <div class="container-input">
       ${input.join('')}
   </div>
@@ -16,7 +16,7 @@ const inputbox = `
   </div>
   <span class='bar_bindcarnum'></span>
  </div>
-`
+`;
 
 const keyboard = [
   [
@@ -24,18 +24,18 @@ const keyboard = [
     ['粤', '川', '津', '沪', '渝', '蒙', '新', '藏', '宁', '桂'],
     ['陕', '浙', '晋', '冀', '青', '鲁', '豫', '苏', '皖', '辽'],
     ['贵', '闽', '赣', '湘', '鄂', '京', '琼', '甘', '云', '黑'],
-    ['吉', '', '', '', '', '', '', '', '', 'delete'],
+    ['吉', '', '', '', '', '', '', '', '', 'delete']
   ],
   [
     ['确定'],
     ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'P', '港', '澳'],
     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '学'],
-    ['', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '', 'delete'],
-  ],
-]
+    ['', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '', 'delete']
+  ]
+];
 
 export default {
   keyboard,
-  inputbox,
-}
+  inputbox
+};
